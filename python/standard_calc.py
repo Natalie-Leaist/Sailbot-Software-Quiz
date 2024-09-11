3fd2c2496452ea7a1abdf293bcab090f42ff65e6
Natalie-Leaist <83516462+Natalie-Leaist@users.noreply.github.com>
2024-09-10 18:10:53 -0700

    Update standard_calc.py

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -1,30 +1,29 @@
 def bound_to_180(angle):
-    #bound the angle to be with 360 degrees
+    # bound the angle to be with 360 degrees
     angle %= 360
-    
-    #if the angle is greater than 180, transform into the corresponding negative value
+
+    # if the angle > 180, transform into the corresponding negative value
     if angle >= 180:
         angle -= 360
-    
+
     return angle
 
 
 def is_angle_between(first_angle, middle_angle, third_angle):
-    #bound all angles to 180
+    # bound all angles to 180
     first_angle = bound_to_180(first_angle)
     middle_angle = bound_to_180(middle_angle)
     third_angle = bound_to_180(third_angle)
-    
+
     isBetween = False
-    
-    #determine the larger and smaller of the first and third angles, in order to determine if the middle angle lies between
+
+    # determine the larger and smaller of the first and third angles
+    # in order to determine if the middle angle lies between
     larger_angle = max(first_angle, third_angle)
     smaller_angle = min(first_angle, third_angle)
-    
-    #check if angle is between the two other angles, assuming non-inclusive of endpoints.
+
+    # check if angle is between the two other angles, assuming non-inclusive.
     if smaller_angle < middle_angle and middle_angle < larger_angle:
         isBetween = True
 
     return isBetween
-
-
